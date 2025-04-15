512f694fd72d2a262d879ded3aa90dbd157f81e2
Pedro Ramos <poramos2001@gmail.com>
2025-04-15 11:00:22 +0200

    Improved the word_frequency function

--- whatsapp_tools.py
+++ whatsapp_tools.py
@@ -27,14 +27,14 @@ def parse_whatsapp_chat(file_path):
     contains, in order, date, time, sender, message.
 
     Parameters:
-    file_path (str): The file path of the txt file, can be either a relative
-        path, an absolute path or a pathlib Path Object.
+        file_path (str): The file path of the txt file, can be either a
+            relative path, an absolute path or a pathlib Path Object.
 
     Returns:
-    list of tuples: Each tuple contains one message found in the file. The
-        message is broken down according to one of the three following
-        whatsapp regular expressions: iOS, Android or POCO. Each tuple has
-        four elements: date, time, sender and message.
+        messages (list of tuples): Each tuple contains one message found in the
+            file. The message is broken down according to one of the three
+            following whatsapp regular expressions: iOS, Android or POCO.
+            Each tuple has four elements: date, time, sender and message.
     """
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
@@ -68,6 +68,18 @@ def parse_whatsapp_chat(file_path):
 
 
 def count_each_sender_date(messages):
+    """
+    Counts the number of messages sent by each user on each day.
+
+    Parameters:
+        messages (list of tuples): Each tuple contains one message broken down
+            in the following elements: date, time, sender and message.
+
+    Returns:
+        ds_df (pd.dataframe): Indexed by dates and each column is one sender
+            present in the input list. Each element is the number of messages
+            sent by the sender that names the column in the rows date.
+    """
     # Delete useless data and turns the list into a df
     messages = [(msg[0], msg[2]) for msg in messages]
 
@@ -89,17 +101,26 @@ def count_each_sender_date(messages):
 
 
 def count_messages_by_sender(messages):
+    """
+    Returns a Counter with the total number of messages sent by each sender.
+    """
     senders = [msg[2] for msg in messages]
     sender_counts = Counter(senders)
     return sender_counts
 
 
 def get_day_of_week(date_string):
+    """
+    Returns the day of the week corresponding to the input date string.
+    """
     date = datetime.datetime.strptime(date_string, '%d/%m/%Y').date()
     return date.strftime('%A')
 
 
 def get_hour(time_str):
+    """
+    Returns the hour present in the input time string.
+    """
     # Convert the string to a datetime object
     try:
         time_obj = datetime.datetime.strptime(time_str, '%H:%M:%S')
@@ -109,7 +130,10 @@ def get_hour(time_str):
     return time_obj.hour
 
 
-def split_and_count(text, word):
+def count_word(text, word):
+    """
+    Counts the number of times the input word appears in the input text.
+    """
     # Split the string using the pattern
     split_text = re.split(r'(\W)', text)
     # Remove empty strings from the result
@@ -123,7 +147,6 @@ def word_frequency(messages,
                    by='weekday',
                    time_period=[8, 18],
                    group_name=None):
-
     # Delete system messages
     if group_name is None:
         group_name = messages[0][2]
@@ -142,33 +165,36 @@ def word_frequency(messages,
         messages_df = pd.DataFrame(messages, columns=['Time', 'Message'])
         messages_df['Time'] = messages_df['Time'].apply(get_hour)
 
+        # testar se o time period n excede o dia
         time_range = list(range(time_period[0], time_period[1]+1))
         messages_df = messages_df[messages_df['Time'].isin(time_range)]
         messages_df = messages_df.reset_index(drop=True)
 
     else:
-        print("Error: by input must be a weekday or hour")
-        return
+        raise ValueError("The 'by' input must be 'weekday' or 'hour'")
 
     # Count the appearances of the words
     messages_df['Message'] = messages_df['Message'].str.lower()
 
-    if isinstance(words, str):
-        word = words.lower()
+    if isinstance(words, (str, int, float, complex, bool)):
+        word = str(words)
+        word = word.lower().strip()
 
         messages_df[word] = messages_df['Message'].apply(
-            lambda x: split_and_count(x, word))
+            lambda x: count_word(x, word))
 
-    elif isinstance(words, list):
-        words = [word.lower() for word in words]
+    elif isinstance(words, (list, tuple, range)):
+        words = [str(word).lower().strip() for word in words]
 
         for word in words:
             messages_df[word] = messages_df['Message'].apply(
-                lambda x: split_and_count(x, word))
+                lambda x: count_word(x, word))
 
     else:
-        print("Error: Words input must be a string or a list of strings")
-        return
+        raise TypeError(
+            "The 'words' input must be a variable or a list/tuple/range of "
+            " variables of the following types: str, int, float, complex"
+            " or bool.")
 
     messages_df.pop('Message')
 
